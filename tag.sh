bce880a98aec4923d2b90c3fdcc47088bdf368c5
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-11-13 15:37:00 +0100

    Make tag.sh robust by checking if a tag exist

--- tag.sh
+++ tag.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Get the current date
 current_date=$(date +"%Y-%m-%d")
+tag_name="v$current_date"
 
-# Create the annotated tag
-git tag -a "v$current_date" -m "Tag v$current_date"
-
-echo "Created annotated tag 'v$current_date'"
+# Check if the tag already exists
+if git rev-parse "$tag_name" >/dev/null 2>&1; then
+  echo "ERROR: Tag '$tag_name' already exists." >&2
+  echo -n "You can delete it first: " >&2
+  echo "git tag -d $tag_name && git push origin --delete $tag_name" 2>&1
+  exit 1
+else
+  # Create the annotated tag if it doesn't exist
+  git tag -a "$tag_name" -m "Tag $tag_name"
+  echo "Created annotated tag '$tag_name'"
+fi
